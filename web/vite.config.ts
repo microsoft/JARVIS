e12956e93f9d7398dc56138392a704f494dcc864
beholdenkey <justthered63@gmail.com>
2023-04-05 09:40:53 -0400

    Update vite.config.ts
    fixed white spaces in code

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,16 @@
 import { fileURLToPath, URL } from "node:url";
-import path  from "path";
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { 
+  server: {
     host: "0.0.0.0",
-    port: 9999, 
-    open: true, 
+    port: 9999,
+    open: true,
     cors: true,
-  },  
+  },
   plugins: [vue()],
   base: "./",
   resolve: {
